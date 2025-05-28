60b1cb4eefa20d87d13a9b68ef736b62dab78f3a
hthuong204 <dtc225270336@ictu.edu.vn>
2025-05-28 22:39:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Gửi crush của tớ",
-    introTitle: 'Crush yêu dấu',
+    titleWeb: "Gửi anhiu DChien nhó",
+    introTitle: 'Bé iu HThuong nèe',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
+    btnIntro: '^^HeHe^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Thoát web là chấp nhận yêu tớ rồi nhé!!!',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    desc: 'anh có iu iem hônggggggg!!!',
+    btnYes: 'Ghét  <33',
+    btnNo: 'iu nhắm :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay, mình qua đón đi chơi nha.',
+    mess: 'em biết mà 🥰. Yêu anh nhiều nhiều 😘😘',
+    messDesc: 'Tối mai đi chụp photobooth nhaaaa nhaaa.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://facebook.com/dinhkaito' //link mess của các bạn. VD: /dinhkaito
 }
